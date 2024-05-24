03e768ee2e56ba2352a3ec858a274d0788517d36
Willy Tan <weeli@mavericks-consulting.com>
2024-05-24 15:16:53 +0800

    Hi

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { json } from 'body-parser';
+import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
 
-import employeeRoutes from './routes/employees';
+import employeeRoutes from "./routes/employees";
 
 const app = express();
 
 app.use(json());
 
-app.use('/employee', employeeRoutes);
+app.use("/employee", employeeRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ "errorMessage": "string" });
+  res.status(500).json({ errorMessage: "string" });
 });
 
-app.listen(3000, ()=>console.log('hey'));
+app.listen(3000, () => console.log("omgggg"));
